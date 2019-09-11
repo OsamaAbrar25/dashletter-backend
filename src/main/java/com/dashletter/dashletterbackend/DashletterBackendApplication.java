8e07a77cdf9204cb6921a9d02ed87d1e9a48f228
Osama Abrar <osama.0035@gmail.com>
2019-09-11 22:25:28 +0530

    Initial Commit

---
+++ src/main/java/com/dashletter/dashletterbackend/DashletterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dashletter.dashletterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashletterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashletterBackendApplication.class, args);
+	}
+
+}
