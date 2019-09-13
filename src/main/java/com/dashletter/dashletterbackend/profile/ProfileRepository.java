36fe5919b692e9afd411ffffa7c36f840f91bbae
Osama Abrar <osama.0035@gmail.com>
2019-09-14 01:17:33 +0530

    Deleted profile repository which was pulled from Branch1.

--- src/main/java/com/dashletter/dashletterbackend/profile/ProfileRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dashletter.dashletterbackend.profile;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProfileRepository extends CrudRepository<Profile, String> {
-}
