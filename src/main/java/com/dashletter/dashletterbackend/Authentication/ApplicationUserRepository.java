b808f5146eb9b101313b64015924150c5cf68496
ZnS-20 <mohda698@gmail.com>
2019-10-22 17:08:14 +0530

    Added Session Security for User.

--- src/main/java/com/dashletter/dashletterbackend/Authentication/ApplicationUserRepository.java
+++ src/main/java/com/dashletter/dashletterbackend/Authentication/ApplicationUserRepository.java
@@ -3,8 +3,11 @@ package com.dashletter.dashletterbackend.Authentication;
 import com.dashletter.dashletterbackend.Models.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ApplicationUserRepository extends JpaRepository<UserProfile, Long> {
 
-    UserProfile findByUsername(String username);
+    Optional<UserProfile> findByUsername(String username);
+    //UserProfile findByUsername(String username);
 
 }
